b8964d7f19780f734405796be3a279d07d4dab2b
Samuel Soares <samuelsoares179@gmail.com>
2022-01-20 20:23:47 -0300

    Adicionando property id na classe cliente

--- source/model/entidades/uCliente.pas
+++ source/model/entidades/uCliente.pas
@@ -8,17 +8,22 @@ type
     Fdocumento: String;
     FdtNascimento: TDate;
     Fnome: String;
+    Fid: Integer;
     procedure Setdocumento(const Value: String);
     procedure SetdtNascimento(const Value: TDate);
     procedure Setnome(const Value: String);
+    procedure Setid(const Value: Integer);
   public
+    property id   : Integer read Fid write Setid;
     property nome : String read Fnome write Setnome;
     property dtNascimento : TDate read FdtNascimento write SetdtNascimento;
     property documento : String read Fdocumento write Setdocumento;
-
   end;
 
 implementation
+
+uses
+  Rest.JSON;
 
 { TCliente }
 
@@ -30,6 +35,11 @@ end;
 procedure TCliente.SetdtNascimento(const Value: TDate);
 begin
   FdtNascimento := Value;
+end;
+
+procedure TCliente.Setid(const Value: Integer);
+begin
+  Fid := Value;
 end;
 
 procedure TCliente.Setnome(const Value: String);
