e052bbc2c38816955cac39f92fa8b1c4956700bd
Samuel Soares <samuelsoares179@gmail.com>
2022-01-19 22:41:27 -0300

    Adicionando data module de conexão a base de dados

---
+++ source/model/dao/uDMConexao.dfm
@@ -0,0 +1,35 @@
+object DMConexao: TDMConexao
+  OldCreateOrder = False
+  Height = 151
+  Width = 197
+  object FDGUIxWaitCursor: TFDGUIxWaitCursor
+    Provider = 'Forms'
+    Left = 41
+    Top = 79
+  end
+  object Transaction: TFDTransaction
+    Options.AutoStop = False
+    Options.DisconnectAction = xdRollback
+    Connection = FDConnection
+    Left = 128
+    Top = 24
+  end
+  object FDConnection: TFDConnection
+    Params.Strings = (
+      'Database=C:\BD\MEGASYS.FDB'
+      'DriverID=FB'
+      'Password=masterkey'
+      'Port=3050'
+      'Server=127.0.0.1'
+      'User_Name=SYSDBA')
+    LoginPrompt = False
+    Transaction = Transaction
+    UpdateTransaction = Transaction
+    Left = 40
+    Top = 24
+  end
+  object DriverLink: TFDPhysFBDriverLink
+    Left = 127
+    Top = 79
+  end
+end
---
+++ source/model/dao/uDMConexao.pas
@@ -0,0 +1,107 @@
+unit uDMConexao;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Classes,
+
+  FireDAC.UI.Intf,
+  FireDAC.VCLUI.Wait,
+  FireDAC.Stan.Intf,
+  FireDAC.Stan.Option,
+  FireDAC.Stan.Error,
+  FireDAC.Phys.Intf,
+  FireDAC.Stan.Def,
+  FireDAC.Stan.Pool,
+  FireDAC.Stan.Async,
+  FireDAC.Phys,
+  FireDAC.Phys.FB,
+  FireDAC.Phys.FBDef,
+  FireDAC.Phys.IBBase,
+  FireDAC.Comp.Client,
+  FireDAC.Comp.UI,
+
+  Data.DB,
+
+  uFuncoesGerais;
+
+type
+  TDMConexao = class(TDataModule)
+    FDGUIxWaitCursor: TFDGUIxWaitCursor;
+    Transaction: TFDTransaction;
+    FDConnection: TFDConnection;
+    DriverLink: TFDPhysFBDriverLink;
+  private
+    path : String;
+  public
+    function criarQry: TFDQuery;
+    procedure conectarBD();
+  end;
+
+var
+  DMConexao: TDMConexao;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+function TDMConexao.criarQry: TFDQuery;
+var
+  qry : TFDQuery;
+begin
+
+  try
+
+    qry := TFDQuery.Create(nil);
+
+    qry.Connection := FDConnection;
+
+    Result := qry;
+
+  except
+    on e:exception do
+      raise Exception.Create(e.Message);
+  end;
+
+end;
+
+procedure TDMConexao.conectarBD();
+var
+  caminho : String;
+begin
+
+  try
+
+    caminho := ParamStr(0);
+    caminho := ExtractFilePath(caminho);
+
+    DriverLink.VendorLib := caminho + 'fbclient.dll';
+
+    with FDConnection do
+    begin
+
+      Connected := false;
+      Params.Clear;
+      Params.Values['DriverID']  := 'FB';
+      Params.Values['Server']    := LerIni('FirebirdConnection','Server');
+      Params.Values['Database']  := LerIni('FirebirdConnection','DatabasePath','');
+      Params.Values['User_name'] := LerIni('FirebirdConnection','User_Name');
+      Params.Values['Password']  := LerIni('FirebirdConnection','Password');
+      Params.Values['Port']      := LerIni('FirebirdConnection', 'Port');
+      Connected := True;
+
+    end;
+
+  except
+    on e:exception do
+      raise Exception.Create('Ocorreu uma Falha na configuração no Banco Firebird!' + e.Message);
+  end;
+
+
+end;
+
+
+end.
