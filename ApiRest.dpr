4073695b637103bf54faeb5916423334fd6f97e9
Samuel Soares <samuelsoares179@gmail.com>
2022-01-22 17:14:42 -0300

    Adicionanado basic auth no projeto

--- ApiRest.dpr
+++ ApiRest.dpr
@@ -16,20 +16,28 @@ uses
   uFuncoesGerais in 'source\lib\uFuncoesGerais.pas',
   uProduto in 'source\model\entidades\uProduto.pas',
   uModel.Produto in 'source\model\uModel.Produto.pas',
-  uController.Produto in 'source\controller\uController.Produto.pas';
+  uController.Produto in 'source\controller\uController.Produto.pas',
+  uVendaItem in 'source\model\entidades\uVendaItem.pas',
+  uVenda in 'source\model\entidades\uVenda.pas',
+  uController.Venda in 'source\controller\uController.Venda.pas',
+  uModel.Venda in 'source\model\uModel.Venda.pas',
+  uModel.VendaItem in 'source\model\uModel.VendaItem.pas',
+  uController.VendaItem in 'source\controller\uController.VendaItem.pas';
 
 begin
 
   THorse.Use(Jhonson());
 
-//  THorse.Use(HorseBasicAuthentication(
-//  function(const AUsername, APassword: string): Boolean
-//  begin
-//    Result := AUsername.Equals('samuel') and APassword.Equals('1234');
-//  end));
+  THorse.Use(HorseBasicAuthentication(
+  function(const AUsername, APassword: string): Boolean
+  begin
+    Result := AUsername.Equals('testserver') and APassword.Equals('testserver');
+  end));
 
   uController.Cliente.Registry;
   uController.Produto.Registry;
+  uController.Venda.Registry;
+  uController.VendaItem.Registry;
 
   THorse.Listen(9000);
 
